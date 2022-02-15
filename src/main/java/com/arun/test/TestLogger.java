309092889873f6e1ff9aa8297ed37bd703cd3c8e
Arun <aneja.arun1@gmail.com>
2022-02-16 00:42:57 +0530

    Adding handlers in LogUtil

---
+++ src/main/java/com/arun/test/TestLogger.java
@@ -0,0 +1,13 @@
+package com.arun.test;
+import com.arun.utils.*;
+
+public class TestLogger {
+
+  public static void main(String[] args) {
+    LogUtil.setHandler();
+    System.out.println(LogUtil.consoleHandler.getLevel());
+    System.out.println(LogUtil.fileHandler.getLevel());
+
+  }
+
+}
--- src/main/java/com/arun/utils/LogUtil.java
+++ src/main/java/com/arun/utils/LogUtil.java
@@ -1,5 +1,29 @@
 package com.arun.utils;
 
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 public class LogUtil {
+  private static final Logger LOGGER = Logger.getLogger(LogUtil.class.getName());
+  public static Handler consoleHandler = null;
+  public static Handler fileHandler = null;
+  
+  public static void setHandler() {
+    try {
+      consoleHandler = new ConsoleHandler();
+      fileHandler = new FileHandler("./logFile.log");
+      consoleHandler.setLevel(Level.ALL);
+      fileHandler.setLevel(Level.ALL);
+    }catch(IOException exception) {
+      LOGGER.log(Level.SEVERE, "Error occured in FileHandler.", exception);
+    }
+  }
+  
+  
 
 }
